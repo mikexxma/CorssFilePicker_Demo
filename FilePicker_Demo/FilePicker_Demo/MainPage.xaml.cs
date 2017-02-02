77c305163b8b69de2672a286ea2c4ce6b8b84e62
v-mxiaox <v-mxiaox@CNV-MXIAOX-W10>
2017-02-02 13:03:18 +0800

    Add project files.

---
+++ FilePicker_Demo/FilePicker_Demo/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Plugin.FilePicker;
+using Plugin.FilePicker.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FilePicker_Demo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OpenFile(object sender, EventArgs e)
+        {
+            try
+            {
+                FileData filedata = await CrossFilePicker.Current.PickFile();
+            }
+            catch (Exception ex)
+            {
+               
+            }
+        }
+    }
+}
